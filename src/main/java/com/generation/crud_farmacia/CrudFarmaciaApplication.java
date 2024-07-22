3617d181ab9571154128de6e99afc23b81543ffa
FShinoda <fernanda.shinoda@hotmail.com>
2024-07-22 09:32:11 -0300

    feat: spring project configured

---
+++ src/main/java/com/generation/crud_farmacia/CrudFarmaciaApplication.java
@@ -0,0 +1,13 @@
+package com.generation.crud_farmacia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudFarmaciaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudFarmaciaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/generation/crud_farmacia/CrudFarmaciaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.generation.crud_farmacia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudFarmaciaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
